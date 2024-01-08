0342702664c0e1baa04f5dd688a5b0be260f6284
chamuda-arangalla <chamudaarangalla@gmail.com>
2024-01-08 11:49:33 +0530

    Add post middleware .

--- backend/controllers/homeController.js
+++ backend/controllers/homeController.js
@@ -2,6 +2,7 @@
 // @route GET api/goals
 // @access Private
 const getGoals = (req,res) => {
+    
     res.status(200).json({ messege:"get goals" })
 }
 
@@ -9,7 +10,7 @@ const getGoals = (req,res) => {
 // @route POST api/goals
 // @access Private
 const setGoal = (req,res)=>{
-    
+    console.log(req.body)
     res.status(200).json({ messege:"set goal" })
 }
 
