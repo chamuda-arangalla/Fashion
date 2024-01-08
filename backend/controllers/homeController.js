a8e7aede149c67ba662cea3df230fb54c2b25351
chamuda-arangalla <chamudaarangalla@gmail.com>
2024-01-08 11:32:40 +0530

    Add comments to controller .

--- backend/controllers/homeController.js
+++ backend/controllers/homeController.js
@@ -1,17 +1,29 @@
+// @desc GET GOALS
+// @route GET api/goals
+// @access Private
 const getGoals = (req,res) => {
     res.status(200).json({ messege:"get goals" })
 }
 
+// @desc SET GOAL
+// @route POST api/goals
+// @access Private
 const setGoal = (req,res)=>{
     
     res.status(200).json({ messege:"set goal" })
 }
 
+// @desc UPDATE GOAL
+// @route PUT api/goals/id
+// @access Private
 const updateGoal = (req,res)=>{
     
     res.status(200).json({ messege:`update goal - ${req.params.id}` })
 }
 
+// @desc DELETE GOAL
+// @route DELETE api/goals/id
+// @access Private
 const deleteGoal = (req,res)=>{
     
     res.status(200).json({ messege:`delete goal - ${req.params.id}` })
