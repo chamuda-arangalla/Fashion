2a170428f4d1b7dbee7d00590c67dfbaafa0fc7e
chamuda-arangalla <chamudaarangalla@gmail.com>
2024-02-06 21:40:36 +0530

    Create User update and delete function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -56,10 +56,38 @@ const updateUser = asyncHandler(async(req,res)=>{
   const {id, username, password, roles, firstName, lastName, address, contactNo, email } = req.body
 
   // Confirm data
-  if(!id || !username || !password || !Array.isArray(roles) || !roles.length || !firstName || !lastName || !address || !contactNo || !email){
-      return res.status(400).json({ messege:'All fields are required'})
+  // if(!id || !username || !password || !Array.isArray(roles) || !roles.length ){
+  //     return res.status(400).json({ messege:'Required fields are not filled'})
+  // }
+  
+  const user = await User.findById(id).exec()
+
+  if(!user){
+    return res.status(400).json({ messege: 'User not found'})
+  }
+
+  const duplicate = await User.findOne({ username }).lean().exec()
+  if(duplicate && duplicate?._id.toString() !== id){
+    return res.status(409).json({ messege: 'Duplicate username' })
+  }
+
+  user.username = username
+  user.roles = roles
+  user.firstName = firstName
+  user.lastName = lastName
+  user.address = address
+  user.contactNo = contactNo
+  user.email = email
+
+  if(password){
+    user.password = await bcrypt.hash(password, 10)
   }
   
+  const updatedUser = await user.save()
+  
+  res.json({ messege: `${updatedUser.username} updated`})
+ 
+
 })
 
 // @desc Delete User
@@ -67,6 +95,22 @@ const updateUser = asyncHandler(async(req,res)=>{
 // @access Private
 const deleteUser = asyncHandler(async(req,res)=>{
   
+  const { id } = req.body
+
+  if(!id){
+    return res.status(400).json({ messege: 'User ID Required'})
+  }
+
+  const user = await User.findById(id).exec()
+
+  if(!user){
+    return res.status(400).json({ messege: 'User Not Found'})
+  }
+
+  await user.deleteOne()
+  const reply = `Username ${user.username} with ID ${user._id}`
+
+  res.json(reply)
 })
   
 module.exports = {
